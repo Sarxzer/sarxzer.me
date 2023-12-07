9422925c572ba8a0175c67a2257e3c032b7da60e
Sarxzer <80457483+Sarxzer@users.noreply.github.com>
2023-12-07 14:25:33 +0100

    7/12/2023 14h25

--- src/js/script.js
+++ src/js/script.js
@@ -36,4 +36,49 @@ function copyCode() {
       console.error('Could not copy text: ', err);
     }
     textarea.remove(); // clean up
-  }
+}
+
+function timeSince(date,type) {
+    const now = new Date();
+    const providedDate = new Date(date);
+    const diffInSeconds = Math.floor((now - providedDate) / 1000);
+
+    if (type === 'auto') {
+        if (Math.floor(diffInSeconds / 31536000) >= 1) {
+            return Math.floor(diffInSeconds / 31536000) + ' years';
+        }
+        if (Math.floor(diffInSeconds / 2592000) >= 1) {
+            return Math.floor(diffInSeconds / 2592000) + ' Months';
+        }
+        if (Math.floor(diffInSeconds / 86400) >= 1) {
+            return Math.floor(diffInSeconds / 86400) + ' days';
+        }
+        if (Math.floor(diffInSeconds / 3600) >= 1) {
+            return Math.floor(diffInSeconds / 3600) + ' hours';
+        }
+        if (Math.floor(diffInSeconds / 60) >= 1) {
+            return Math.floor(diffInSeconds / 60) + ' minutes';
+        }
+        return Math.floor(diffInSeconds) + ' secondes';
+
+    }else{
+        if (type === 'years') {
+            return Math.floor(diffInSeconds / 31536000);
+        }
+        if (type === 'months') {
+            return Math.floor(diffInSeconds / 2592000);
+        }
+        if (type === 'days') {
+            return Math.floor(diffInSeconds / 86400);
+        }
+        if (type === 'hours') {
+            return Math.floor(diffInSeconds / 3600);
+        }
+        if (type === 'minutes') {
+            return Math.floor(diffInSeconds / 60);
+        }
+        return Math.floor(diffInSeconds);
+    }
+
+
+}
